01668edd585610973c1555349b58443d01fc568d
RalphHan <hanhongwei_hf@qq.com>
2023-09-13 18:33:44 +0800

    add ik_all

--- ik_all.py
+++ ik_all.py
@@ -9,6 +9,7 @@ from data_loaders.humanml.scripts.motion_process import recover_from_ric
 import torch.multiprocessing as mp
 
 n_workers = torch.cuda.device_count()
+batch_size=50
 
 def worker(worker_id):
     files = os.listdir("database/")
@@ -23,8 +24,10 @@ def worker(worker_id):
     block_size = (len(motions) + n_workers - 1) // n_workers
     start = worker_id * block_size
     end = start + block_size
+    motions=motions[start:end]
     os.makedirs("motion_database/", exist_ok=True)
-    for motion_file in tqdm(motions[start:end]):
+    batch=[]
+    for motion_file in tqdm(motions):
         joints = torch.tensor(np.load(f"database/{motion_file}"))
         n_joints = 22
         joints = recover_from_ric(joints, n_joints).numpy()
@@ -34,19 +37,22 @@ def worker(worker_id):
         if (joints[:, 9, 1] > joints[:, 0, 1]).sum() / joints.shape[0] > 0.85 \
                 and (cosine > -0.2).sum() / joints.shape[0] < 0.5:
             joints[..., 0] = -joints[..., 0]
-        rotations, root_pos = j2s([joints], step_size=2e-2, num_iters=30, optimizer="lbfgs")
-        rotations = rotations[0]
-        root_pos = root_pos[0]
-        with open(f"motion_database/{motion_file.replace('.npy', '.json')}", "w") as f:
-            json.dump({"root_positions": binascii.b2a_base64(
-                root_pos.flatten().astype(np.float32).tobytes()).decode("utf-8"),
-                       "rotations": binascii.b2a_base64(rotations.flatten().astype(np.float32).tobytes()).decode(
-                           "utf-8"),
-                       "dtype": "float32",
-                       "fps": 20,
-                       "mode": "axis_angle",
-                       "n_frames": rotations.shape[0],
-                       "n_joints": 24}, f, indent=4)
+        batch.append((motion_file,joints))
+        if len(batch)<batch_size and motion_file!=motions[-1]:
+            continue
+        all_rotations, all_root_pos = j2s([x[1] for x in batch], step_size=2e-2, num_iters=30, optimizer="lbfgs")
+        for rotations, root_pos, file in zip(all_rotations, all_root_pos, [x[0] for x in batch]):
+            with open(f"motion_database/{file.replace('.npy', '.json')}", "w") as f:
+                json.dump({"root_positions": binascii.b2a_base64(
+                    root_pos.flatten().astype(np.float32).tobytes()).decode("utf-8"),
+                           "rotations": binascii.b2a_base64(rotations.flatten().astype(np.float32).tobytes()).decode(
+                               "utf-8"),
+                           "dtype": "float32",
+                           "fps": 20,
+                           "mode": "axis_angle",
+                           "n_frames": rotations.shape[0],
+                           "n_joints": 24}, f, indent=4)
+        batch.clear()
 
 
 if __name__ == '__main__':
