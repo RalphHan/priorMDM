99e81fb8112c48a24121689cb9f956988c907bf9
RalphHan <hanhongwei_hf@qq.com>
2024-02-19 20:33:57 +0800

    add ik_ntu

--- ik_ntu/ik.py
+++ ik_ntu/ik.py
@@ -8,7 +8,7 @@ import torch
 from .visualize import Joints2SMPL
 import os
 from tqdm import tqdm
-from data_loaders.humanml.scripts.motion_process import recover_from_ric
+from data_loaders.humanml.common.quaternion import qbetween_np
 import sys
 import random
 from .txt2npy import _read_skeleton
@@ -42,7 +42,27 @@ def worker(worker_id, n_workers):
                 pass
         try:
             joints = _read_skeleton(f"skeletons/{motion_file}")['skel_body0'].astype(np.float32)
+            '''Put on Floor'''
+            floor_height = joints.min(axis=0).min(axis=0)[1]
+            joints[..., 1] -= floor_height
+
+            '''XZ at origin'''
             joints[..., [0, 2]] -= joints[0, 0, [0, 2]]
+
+            '''All initially face Z+'''
+            root_pos_init = joints[0]
+            r_hip, l_hip, sdr_r, sdr_l = [16, 12, 8, 4]
+            across1 = root_pos_init[r_hip] - root_pos_init[l_hip]
+            across2 = root_pos_init[sdr_r] - root_pos_init[sdr_l]
+            across = across1 + across2
+            across = across / np.sqrt((across ** 2).sum(axis=-1))[..., np.newaxis]
+            forward_init = np.cross(np.float32([[0, 1, 0]]), across, axis=-1)
+            forward_init = forward_init / np.sqrt((forward_init ** 2).sum(axis=-1))[..., np.newaxis]
+            target = np.float32([[0, 0, 1]])
+            root_quat_init = qbetween_np(forward_init, target).astype(np.float32)
+            root_quat_init = np.ones(joints.shape[:-1] + (4,), dtype=np.float32) * root_quat_init
+            joints = qrot_np(root_quat_init, joints)
+            print(joints.shape, joints.dtype)
         except:
             with open("failed.txt", "a") as f:
                 f.write(f"{motion_file}\n")
